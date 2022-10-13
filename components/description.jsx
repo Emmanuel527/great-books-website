aeb4523285e3a5cd8936f20cdd1ce58fb9f31bc9
Emmanuel527 <arowoseluemmanuel@gmail.com>
2022-10-12 22:49:46 -0500

    v1.0

--- components/description.jsx
+++ components/description.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const description = () => {
   return (
-    <div className='m-10 font-bold text-2xl text-white'> <p> I want to read the greatest books in human history. </p> These books contain the ideas that have driven most of human history, knowledge and progress. Reading them enables one to understand and take part in the dialogue of mankind.<p>This website will be updated regurlarly with posts on my thoughts, perspectives and the lessons I'm learning from whatever book I am currently reading through. The website is not complete and is still in development.</p></div>
+    <div className='m-10 font-bold text-2xl text-white'> <p> I want to read the greatest books of all time. </p> These books contain the ideas that have driven most of human history, knowledge and progress. Reading them enables one to understand and take part in the dialogue of mankind.<p>This website will be updated regurlarly with posts on my thoughts, perspectives and the lessons I'm learning from whatever book I am currently reading through. The website is not complete and is still in development.</p></div>
   )
 }
 
