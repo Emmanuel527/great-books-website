41c9ee4f763fe9b507ca1e143904ceb3bf99fef5
Emmanuel527 <arowoseluemmanuel@gmail.com>
2022-10-12 22:15:27 -0500

    v1.0

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div class="flex justify-center text-3xl m-10 font-[1000] text-white">
-       <h1>Emmanuel's Literary Canon Project</h1>
+       <h1>LITERARY CANON PROJECT</h1>
     </div>
   )
 }
--- components/categories.jsx
+++ components/categories.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const categories = () => {
   return (
     <div className='flex justify-center p-10 text-5xl text-white'> 
-      Genres 
+      GENRES 
     </div>
   )
 }
