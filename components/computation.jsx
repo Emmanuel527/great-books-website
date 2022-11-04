8bc6be5d0008da6b169355966a7ad609833452fd
Emmanuel527 <arowoseluemmanuel@gmail.com>
2022-11-04 12:38:58 -0500

    added description

--- components/computation.jsx
+++ components/computation.jsx
@@ -5,6 +5,33 @@ const computation = () => {
   return (
     <><div class='text-white m-5'>Computation is any type of calculation that includes both arithmetical and non-arithmetical steps and follows a well-defined model understood and described as, for example, an algorithm. The study of computation is paramount to the discipline of computer science.</div>
     <Booklist />
+    Deep Medicine
+How Artificial Intelligence Can Make Healthcare Human Again
+
+by Eric Topol, MD
+
+AI Superpowers
+
+China, Silicon Valley and the new world order
+
+The Master Algorithm:
+How the Quest for the Ultimate Learning Machine Will Remake Our World
+
+The Three-Body Problem (novel)
+
+AI 2041: Ten Visions for Our Future
+
+Superintelligence: Paths, Dangers, Strategies 
+
+Human Compatible: Artificial Intelligence and the Problem of Control 
+
+Klara and the Sun
+
+The Age of AI
+And Our Human Future
+
+The Age of Intelligent Machines (1990) and The Age of Spiritual Machines 
+The Singularity Is Near: When Humans Transcend Biology
     </>
   )
 }
