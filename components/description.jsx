b0c6028900d43e9c646ebab4a0bf8114b8064d24
Emmanuel527 <arowoseluemmanuel@gmail.com>
2023-03-16 11:13:18 -0500

    commit

--- components/description.jsx
+++ components/description.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const description = () => {
   return (
-    <div className='m-10 font-bold text-2xl text-white'> <p> Hello, My name is Emmanuel Arowoselu. I want to embark on a multi-year project to read the greatest books of all time. </p> These books contain the ideas that have driven most of human history, knowledge and progress. Reading them enables one to understand and take part in the dialogue of mankind. Every book listed here is worth your time.<p>This website will be updated regurlarly with posts on my thoughts, perspectives and the lessons I'm learning from whatever book I am currently reading through.</p> <p> Currently Reading: <em>The Thirteen Books of Euclid's Elements.</em></p>
+    <div className='m-10 font-bold text-2xl text-white'> <p> Hello, My name is Emmanuel Arowoselu. I want to embark on a multi-year project to read the greatest books of all time. </p> These books contain the ideas that have driven most of human history, knowledge and progress. Reading them enables one to understand and take part in the dialogue of mankind. Every book listed here is worth your time.<p>This website will be updated regurlarly with posts on my thoughts, perspectives and the lessons I'm learning from whatever book I am currently reading through.</p> <p> Currently Reading: <em>Brothers Karamazov</em></p>
     <p class='italic'>"I have to understand the world, you see." - Richard Feynman</p></div>
   )
 }
