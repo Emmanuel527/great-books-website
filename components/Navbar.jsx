3ab31e92293158c434ae3c96c0a875341e4d641c
EmmanuelArowoselu <arowoseluemmanuel@gmail.com>
2023-10-23 19:41:04 -0600

    🚀

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div class="flex justify-center p-5 text-5xl text-black">
-       <h1>ANTONINE</h1>
+    <div className="flex justify-center p-5 text-5xl text-black">
+       <h1 id='heading'>ANTONINE</h1>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+
+      backgroundImage: {
+        'hero-img': "url('/public/images/hero-img.png')",
+      }
+      
+    },
     fontFamily: {
       roman : 'Romanesco'
     }
+
   },
+
   plugins: [],
+
 }
